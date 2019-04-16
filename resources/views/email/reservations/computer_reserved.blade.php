7a559c440eddac2e4c65710dc27f3a9feb220f14
Lemar Gray <lemar@lan.barita.com>
2019-04-16 18:26:29 -0500

    Email confirmation message styling

--- resources/views/email/reservations/computer_reserved.blade.php
+++ resources/views/email/reservations/computer_reserved.blade.php
@@ -1,5 +1,30 @@
-<body>
-    <div>
-        {{$reservation->reservedBy->name}}
-    </div>
-</body>
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            <img src="{{asset('images/utech/icon.png')}}">
+            <br>
+            <h1>{{config('app.name')}}</h1>
+        @endcomponent
+    @endslot
+{{-- Body --}}
+    Hello {{ $reservation->reservedBy->name }},
+
+    <p>Your request to reserve {{$reservation->computer->name}} for the period: {{$reservation->start_date}} - {{$reservation->end_date}}
+        Has been confirmed.
+    </p>
+{{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+{{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ config('app.name') }}. Super FOOTER!
+        @endcomponent
+    @endslot
+@endcomponent
